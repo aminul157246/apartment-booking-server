221ef4a4cfb9fc420903ae6977b19dc3a021f17a
aminul157246 <aminul.islam.157246@gmail.com>
2024-03-20 09:51:07 +0600

    all api created of carts

--- index.js
+++ index.js
@@ -62,17 +62,24 @@ async function run() {
 
 
         app.get('/carts' , async(req, res) => {
-            const email = req.params.email
+            const email = req.query.email
             console.log(email);
-            const query = {email : email}
+            const query = { "apartmentItem.email" : email}
             const result = await cartCollection.find(query).toArray()
-            res.send(result)
+            res.send(result);
         })
 
 
         app.post('/carts' , async(req, res) => {
             const item = req.body
             const result = await cartCollection.insertOne(item)
+            res.send(result)
+        })
+
+        app.get('/carts/:id' , async(req, res) => {
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
         
