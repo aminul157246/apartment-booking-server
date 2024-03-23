682b2559cbda7d2b4b5342df24a26a6bc922f8bb
aminul157246 <aminul.islam.157246@gmail.com>
2024-03-23 23:25:21 +0600

    cart delete api created

--- index.js
+++ index.js
@@ -244,7 +244,12 @@ async function run() {
             const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
-
+app.delete('/carts/:id', async(req, res) => {
+    const id = req.params.id
+    const query = {_id : new ObjectId(id)}
+    const result = await cartCollection.deleteOne(query)
+    res.send(result)
+})
 
 
 
